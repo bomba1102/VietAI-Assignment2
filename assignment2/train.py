76f5f034b589b1c623b4c36ce1c44b6eef45dc94
Hoàng Long <longhoangai1102@gmail.com>
2023-06-18 09:18:35 +0700

    Update train.py

--- assignment2/train.py
+++ assignment2/train.py
@@ -295,7 +295,7 @@ def load_pretrained_model(local_rank, model_path: str = ""):
 
     # Create LoRA model
     model = LoraModelForCasualLM(model, lora_config)
-    # model = get_peft_model(model, lora_config) # Uncomment this line to use PEFT library instead of your implementation in `lora_layer.py`.
+    model = get_peft_model(model, lora_config) # Uncomment this line to use PEFT library instead of your implementation in `lora_layer.py`.
     if _is_master_process():
         model.print_trainable_parameters()
 
